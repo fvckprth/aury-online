c243ea391ff0d37e9dde7882c16e66494be37cf3
parth patel <parth@qortor.com>
2023-07-21 16:50:14 -0400

    Added "memorable sounds" and "A24 films" links to AboutMe

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -14,14 +14,14 @@ function AboutMe() {
         </p>
         <p className="text-xs sm:text-sm tracking-wide font-light mt-4">
             Began my journey somewhere in the middle of medicine,<br/>
-            Shanghai, & DJing. I’m the founder of <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">East Park</a>, an NYC<br/>
+            Shanghai, and DJing. I’m the founder of <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">East Park</a>, an NYC<br/>
             group in the intersection of commerce, media, and climate <br/>
             tech. I’m currently focused on building an easier way to <br/>
             streamline collaborative commerce at <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">P307</a>.
         </p>
         <p className="text-xs sm:text-sm tracking-wide font-light mt-4">
             A slave to romanticism, I practice religiously. I like to spend <br/>
-            my time curating <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">memorable sounds</a>, watching <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">A24 films</a>, <br/>
+            my time curating <a href="https://spotify.link/t90Be9JJCBb" className="underline underline-offset-4 decoration-from-font hover:opacity-25">memorable sounds</a>, watching <a href="https://boxd.it/5ESSx" className="underline underline-offset-4 decoration-from-font hover:opacity-25">A24 films</a>, <br/>
             drinking <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">Old Fashioneds</a>, and cherishing <a href="#" className="underline underline-offset-4 decoration-from-font hover:opacity-25">Sonia</a>.
         </p>
       </div>
